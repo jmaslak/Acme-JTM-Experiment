b63435490308714ae76bfb7df75aba4e4eae3734
Joelle Maslak <jmaslak@antelope.net>
2018-07-27 09:30:24 -0600

    Initial version

---
+++ lib/Acme/JTM/Experiment.pm
@@ -0,0 +1,26 @@
+#!/usr/bin/perl
+
+#
+# Copyright (C) 2018 Joelle Maslak
+# All Rights Reserved - See License
+#
+
+package Acme::JTM::Experiment;
+
+use v5.8;
+
+# ABSTRACT: Testing Perl Constructs on CPAN Testers
+
+use strict;
+use warnings;
+
+=head1 DESCRIPTION
+
+This module is a place where I (Joelle) try
+different Perl constructrs that may have compatibility issues.  By
+trying it with this module, I can test this across CPAN Testers.
+
+=cut
+
+1;
+
---
+++ t/01-Basic.t
@@ -0,0 +1,17 @@
+#!/usr/bin/perl -T
+# Yes, we want to make sure things work in taint mode
+
+#
+# Copyright (C) 2018 Joelle Maslak
+# All Rights Reserved - See License
+#
+
+use strict;
+use warnings;
+
+use Test2::V0;
+
+use Acme::JTM::Experiment;
+
+pass("Successfully loaded Acme::JTM::Experiment");
+
